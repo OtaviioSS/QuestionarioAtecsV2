b517e5251f23fb8a9a44132097f4998f982db8b0
Otavio Siqueira <54749776+OtaviioSS@users.noreply.github.com>
2020-05-02 18:16:20 -0300

    Add files via upload

--- banco.js
+++ banco.js
@@ -6,7 +6,7 @@ class bdSql{
     constructor(){
         this.conexao = window.openDatabase("myData", "1.0", "MyDataBase", 2 * 1024 * 1024);
         this.conexao.transaction(function (tx) {
-            tx.executeSql("CREATE TABLE IF NOT EXISTS  formularios(id integer primary key autoincrement ,plataformaAtual varchar ,nuvemoulocal varchar ,dadosseguros varchar ,atende varchar ,considera varchar ,vlaquisicao varchar ,vlmensal varchar ,manutencaoAtende,ideal varchar, plataformaOnline varchar,vlIdeal)",[], 
+            tx.executeSql("CREATE TABLE IF NOT EXISTS  formularios(id integer primary key autoincrement ,email varchar,telefone ,plataformaAtual varchar ,nuvemoulocal varchar ,dadosseguros varchar ,atende varchar ,considera varchar ,vlaquisicao varchar ,vlmensal varchar ,manutencaoAtende,ideal varchar, plataformaOnline varchar,vlIdeal)",[], 
             function(tx, msg){
                 console.log('Sucesso');
                 console.log(msg);
@@ -21,7 +21,7 @@ class bdSql{
     }
     iserirDado(dados,callback){
         this.conexao.transaction(function (tx) {
-            tx.executeSql('insert into formularios(plataformaAtual,nuvemoulocal,dadosseguros,atende,considera,vlaquisicao,vlmensal,manutencaoAtende,ideal,plataformaOnline,vlIdeal) values(?,?,?,?,?,?,?,?,?,?,?)', dados, 
+            tx.executeSql('insert into formularios(email,telefone,plataformaAtual,nuvemoulocal,dadosseguros,atende,considera,vlaquisicao,vlmensal,manutencaoAtende,ideal,plataformaOnline,vlIdeal) values(?,?,?,?,?,?,?,?,?,?,?,?,?)', dados, 
             function(resultado, msg){
                     callback(resultado)
                  }, 
